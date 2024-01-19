d8c7185ae9f3ca533c2a45087b22a583a0648fb1
Gabriel Moura <gabrielmoura-cma@hotmail.com>
2024-01-19 11:56:44 -0300

    refactor: improving the folder structure

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import RoutesApp from "./routes";
-import { ToastContainer} from "react-toastify";
-import 'react-toastify/dist/ReactToastify.css';
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 function App() {
   return (
     <div className="App">
       <ToastContainer />
-      <RoutesApp/>
+      <RoutesApp />
     </div>
   );
 }
