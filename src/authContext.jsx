72214181a6a98cee7106149de1b6f5ff891d7d63
Gabriel Moura <gabrielmoura-cma@hotmail.com>
2024-01-19 12:08:33 -0300

    feat: created authProvider

---
+++ src/authContext.jsx
@@ -0,0 +1,19 @@
+import { createContext, useState } from "react";
+
+const AuthContext = createContext({});
+
+const AuthProvider = ({ children }) => {
+  const [user, setUser] = useState({});
+
+  return (
+    <AuthContext.Provider
+      value={{
+        user: !!user,
+        setUser,
+      }}
+    >
+      {children}
+    </AuthContext.Provider>
+  );
+};
+export { AuthContext, AuthProvider };
