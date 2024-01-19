153c26a5d83702dd61b8dcf1579382670fc64426
Gabriel Moura <gabrielmoura-cma@hotmail.com>
2024-01-19 20:51:00 -0300

    chore: add firebaseConnection

---
+++ src/services/firebaseConection.js
@@ -0,0 +1,17 @@
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAtm41mzmA8JZZZCNDwbxhoEZiA2PZwdTk",
+  authDomain: "your-movie-2acba.firebaseapp.com",
+  projectId: "your-movie-2acba",
+  storageBucket: "your-movie-2acba.appspot.com",
+  messagingSenderId: "958305882527",
+  appId: "1:958305882527:web:05a143a5e189d52907adaa",
+  measurementId: "G-M1PEEPGV1W",
+};
+
+const firebaseApp = initializeApp(firebaseConfig);
+export const auth = getAuth(firebaseApp);
+export const db = getFirestore(firebaseApp);
