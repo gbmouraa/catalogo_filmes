72331f4e5803869b5f72e2ce4ca509db0b6fbbcb
Gabriel Moura <gabrielmoura-cma@hotmail.com>
2024-01-19 12:08:53 -0300

    feat: add AuthProvider

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
+import { AuthProvider } from "./authContext";
 import RoutesApp from "./routes";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 
 function App() {
   return (
-    <div className="App">
-      <ToastContainer />
-      <RoutesApp />
-    </div>
+    <AuthProvider>
+      <div className="App">
+        <ToastContainer />
+        <RoutesApp />
+      </div>
+    </AuthProvider>
   );
 }
 
