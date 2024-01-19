e7c0cd2a5706008228035501f4fd248ef5d31821
Gabriel Moura <gabrielmoura-cma@hotmail.com>
2024-01-19 13:08:42 -0300

    feat: add SignIn to routes

--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -5,6 +5,7 @@ import Home from "../pages/Home";
 import Filme from "../pages/Filme";
 import Favoritos from "../pages/Favoritos";
 import Erro from "../pages/Erro";
+import SignIn from "../pages/SignIn";
 
 function RoutesApp() {
   return (
@@ -14,6 +15,7 @@ function RoutesApp() {
         <Route path="/" element={<Home />} />
         <Route path="/filme/:id" element={<Filme />} />
         <Route path="/favoritos" element={<Favoritos />} />
+        <Route path="/login" element={<SignIn />} />
         <Route path="*" element={<Erro />} />
       </Routes>
     </BrowserRouter>
