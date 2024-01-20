32647720755e04952847df980a465d5ee1cb77c4
Gabriel Moura <gabrielmoura-cma@hotmail.com>
2024-01-19 22:15:36 -0300

    feat: created User

---
+++ src/components/User/index.jsx
@@ -0,0 +1,42 @@
+import { useContext } from "react";
+import { Link } from "react-router-dom";
+import { AuthContext } from "../../authContext";
+import { FaRegUserCircle } from "react-icons/fa";
+import "./user.css";
+
+const User = () => {
+  const { signed, user } = useContext(AuthContext);
+
+  if (signed) {
+    return (
+      <div className="user">
+        {user.avatarUrl === null ? (
+          <FaRegUserCircle size={32} color="#fff" />
+        ) : (
+          <img src={user.avatarUrl} alt="Avatar" />
+        )}
+
+        <div>
+          <span>Olá, {user.name}</span>
+          <span className="acount-actions">
+            <a>Minha conta</a> | <button>Sair</button>
+          </span>
+        </div>
+      </div>
+    );
+  }
+
+  return (
+    <div className="user">
+      <FaRegUserCircle size={32} color="#fff" />
+      <div className="login-actions">
+        <span>
+          <Link to="/login">Faça login</Link>, ou
+        </span>
+        <Link to="/register">Cadastre-se</Link>
+      </div>
+    </div>
+  );
+};
+
+export default User;
