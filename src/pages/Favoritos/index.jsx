8a1d54b6ecdb3e07c62d584bc7e2a5ae32c0e6e1
Gabriel Moura <gabrielmoura-cma@hotmail.com>
2024-01-19 22:16:06 -0300

    .

--- src/pages/Favoritos/index.jsx
+++ src/pages/Favoritos/index.jsx
@@ -1,42 +1,51 @@
-import { useEffect, useState } from 'react'
-import { Link } from 'react-router-dom'
-import './favoritos.css'
-import {toast} from 'react-toastify'
+import { useEffect, useState } from "react";
+import Header from "../../components/Header";
+import { Link } from "react-router-dom";
+import "./favoritos.css";
+import { toast } from "react-toastify";
 
 function Favoritos() {
-  const [filmes, setFilmes] = useState([])
+  const [filmes, setFilmes] = useState([]);
 
   useEffect(() => {
-    const minhaLista = localStorage.getItem("@yourmovie")
-    setFilmes(JSON.parse(minhaLista) || [])
-  }, [])
+    const minhaLista = localStorage.getItem("@yourmovie");
+    setFilmes(JSON.parse(minhaLista) || []);
+  }, []);
 
   const excluirFilme = (id) => {
-    const filtroFilmes = filmes.filter(item => item.id !== id)
-    setFilmes(filtroFilmes)
-    localStorage.setItem("@yourmovie",JSON.stringify(filtroFilmes))
-    toast.success("Filme excluido com sucesso!")
-  }
+    const filtroFilmes = filmes.filter((item) => item.id !== id);
+    setFilmes(filtroFilmes);
+    localStorage.setItem("@yourmovie", JSON.stringify(filtroFilmes));
+    toast.success("Filme excluido com sucesso!");
+  };
 
   return (
-    <section className='container-favoritos'>
-      <h1>Meus Filmes</h1>
-      {filmes.length === 0 && <span>Você não possui filmes salvos :(</span>}
-      <ul>
-        {filmes.map(filme => {
-          return (
-            <li key={filme.id}>
-              <span className='titulo'>{filme.title}</span>
-              <div>
-                <Link to={`/filme/${filme.id}`}>Detalhes</Link>
-                <button className='btn-excluir' onClick={() => excluirFilme(filme.id)}>Excluir</button>
-              </div>
-            </li>
-          )
-        })}
-      </ul>
-    </section>
-  )
+    <>
+      <Header />
+      <section className="container-favoritos">
+        <h1>Meus Filmes</h1>
+        {filmes.length === 0 && <span>Você não possui filmes salvos :(</span>}
+        <ul>
+          {filmes.map((filme) => {
+            return (
+              <li key={filme.id}>
+                <span className="titulo">{filme.title}</span>
+                <div>
+                  <Link to={`/filme/${filme.id}`}>Detalhes</Link>
+                  <button
+                    className="btn-excluir"
+                    onClick={() => excluirFilme(filme.id)}
+                  >
+                    Excluir
+                  </button>
+                </div>
+              </li>
+            );
+          })}
+        </ul>
+      </section>
+    </>
+  );
 }
 
-export default Favoritos
+export default Favoritos;
