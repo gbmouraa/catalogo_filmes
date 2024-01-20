803c817b2a5b49e13c3ba8ae8e534bd975bd0b42
Gabriel Moura <gabrielmoura-cma@hotmail.com>
2024-01-19 22:19:40 -0300

    feat: add SignUp to routes

--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -1,24 +1,22 @@
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { Route, Routes } from "react-router-dom";
 
-import Header from "../components/Header";
 import Home from "../pages/Home";
 import Filme from "../pages/Filme";
 import Favoritos from "../pages/Favoritos";
 import Erro from "../pages/Erro";
 import SignIn from "../pages/SignIn";
+import SignUp from "../pages/SignUp";
 
 function RoutesApp() {
   return (
-    <BrowserRouter>
-      <Header />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/filme/:id" element={<Filme />} />
-        <Route path="/favoritos" element={<Favoritos />} />
-        <Route path="/login" element={<SignIn />} />
-        <Route path="*" element={<Erro />} />
-      </Routes>
-    </BrowserRouter>
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/filme/:id" element={<Filme />} />
+      <Route path="/favoritos" element={<Favoritos />} />
+      <Route path="/login" element={<SignIn />} />
+      <Route path="/register" element={<SignUp />} />
+      <Route path="*" element={<Erro />} />
+    </Routes>
   );
 }
 
