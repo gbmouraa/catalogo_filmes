e4512e537154b20b69eb48e2bc99f4732fe3fe0a
Gabriel Moura <gabrielmoura-cma@hotmail.com>
2024-01-25 16:09:22 -0300

    feat: add fn to delete movies in fireabse

--- src/pages/Favoritos/index.jsx
+++ src/pages/Favoritos/index.jsx
@@ -1,22 +1,51 @@
-import { useEffect, useState } from "react";
+import { useEffect, useState, useContext } from "react";
+import { AuthContext } from "../../authContext";
+import { db } from "../../services/firebaseConection";
+import { updateDoc, doc } from "firebase/firestore";
 import Header from "../../components/Header";
 import { Link } from "react-router-dom";
 import "./favoritos.css";
 import { toast } from "react-toastify";
 
 function Favoritos() {
+  const { user, setUser, setUserStorage } = useContext(AuthContext);
   const [filmes, setFilmes] = useState([]);
 
   useEffect(() => {
-    const minhaLista = localStorage.getItem("@yourmovie");
-    setFilmes(JSON.parse(minhaLista) || []);
+    function loadFilmes() {
+      let storageData = JSON.parse(localStorage.getItem("@your-movie"));
+      const hasFilmesSalvos = storageData?.filmes;
+      if (hasFilmesSalvos && hasFilmesSalvos.length > 0)
+        setFilmes(hasFilmesSalvos);
+    }
+
+    loadFilmes();
   }, []);
 
-  const excluirFilme = (id) => {
+  const excluirFilme = async (id) => {
     const filtroFilmes = filmes.filter((item) => item.id !== id);
     setFilmes(filtroFilmes);
-    localStorage.setItem("@yourmovie", JSON.stringify(filtroFilmes));
-    toast.success("Filme excluido com sucesso!");
+
+    const uid = user.userID;
+    const docRef = doc(db, "users", uid);
+
+    await updateDoc(docRef, {
+      ...user,
+      filmes: filtroFilmes,
+    })
+      .then(() => {
+        let data = {
+          ...user,
+          filmes: filtroFilmes,
+        };
+        setUserStorage(data);
+        setUser(data);
+        toast.success("Filme excluido com sucesso!");
+      })
+      .catch((error) => {
+        console.log(error);
+        toast.error("Ocorreu um erro tente novamente.");
+      });
   };
 
   return (
