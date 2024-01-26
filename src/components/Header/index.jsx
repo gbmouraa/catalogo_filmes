4008d80900f85bf80d02e5fac190d3ecc8210fb7
Gabriel Moura <gabrielmoura-cma@hotmail.com>
2024-01-25 22:30:43 -0300

    feat: add ModalLogout

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,22 +1,51 @@
+import { useEffect, useState, useContext } from "react";
+import { AuthContext } from "../../authContext";
 import { Link } from "react-router-dom";
 import User from "../User";
 import "./header.css";
+import ModalLogout from "../ModalLogout";
 
 function Header() {
+  const { logOut } = useContext(AuthContext);
+
+  const [modalIsOpen, setModalIsOpen] = useState(false);
+
+  useEffect(() => {
+    if (modalIsOpen) {
+      document.body.style.overflow = "hidden";
+    } else {
+      document.body.style.overflow = "";
+    }
+  }, [modalIsOpen]);
+
+  async function handleLogout() {
+    await logOut();
+    setModalIsOpen(false);
+  }
+
   return (
-    <header>
-      <div className="container">
-        <Link className="logo" to="/">
-          YourMovie<span>.com</span>
-        </Link>
-        <div className="actions">
-          <User />
-          <Link className="meus-filmes" to="/favoritos">
-            Meus Filmes
+    <>
+      <header>
+        <div className="container">
+          <Link className="logo" to="/">
+            YourMovie<span>.com</span>
           </Link>
+          <div className="actions">
+            <Link className="meus-filmes" to="/favoritos">
+              Meus Filmes
+            </Link>
+            <User logout={() => setModalIsOpen(true)} />
+          </div>
         </div>
-      </div>
-    </header>
+      </header>
+
+      {modalIsOpen && (
+        <ModalLogout
+          voltar={() => setModalIsOpen(false)}
+          sair={() => handleLogout()}
+        />
+      )}
+    </>
   );
 }
 
