bcf4b5bb552f3a2d68bbb69762fed36b6c9eba6d
Gabriel Moura <gabrielmoura-cma@hotmail.com>
2024-01-26 15:06:30 -0300

    style: changed toast auto close time

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
   return (
     <BrowserRouter>
       <AuthProvider>
-        <ToastContainer />
+        <ToastContainer autoClose={1400} />
         <RoutesApp />
       </AuthProvider>
     </BrowserRouter>
